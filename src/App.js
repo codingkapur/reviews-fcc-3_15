d95329807d7bfcbfda4967434c71bcea8ca23487
Raghav <codingkapur@gmail.com>
2021-09-19 12:14:44 +0530

    implement data fetch from mock database db.json and update state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import {useState} from 'react'
-
+import {useEffect, useState} from 'react'
 import {FaTimes} from 'react-icons';
+
 function App() {
+
+  const [reviews, setReviews] = useState([])
+
+  const getReviews = async () => {
+    const res = await fetch('http://localhost:5000/reviews');
+    const data = await res.json();
+    return data
+  }
+
+  const fetchReviews = async () => {
+    const reviewData = await getReviews();
+    setReviews(reviewData);
+    console.log(reviews);
+  }
+
+  useEffect(()=> {
+    fetchReviews()
+  }, [])
+  console.log(reviews);
+
   return (
     <main className="container">
       <h1>Hello React: REVIEWS</h1>
