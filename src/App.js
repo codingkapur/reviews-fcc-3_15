962686e3024ebc67bf16e0cf559830171c4720a7
Raghav <codingkapur@gmail.com>
2021-09-19 14:39:46 +0530

    implement functionality of data retrieval differently and add all other functionality

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import {useEffect, useState} from 'react'
 import Review from './components/Review'
 
 function App() {
-  //Define State
-  const [reviews, setReviews] = useState([])
-  //Fetch Data
-  const getReviews = async () => {
-    const res = await fetch('http://localhost:5000/reviews');
-    const data = await res.json();
-    return data
-  }
+  // //Define State
+  // const [reviews, setReviews] = useState([])
+  // //Fetch Data
+  // const getReviews = async () => {
+  //   const res = await fetch('http://localhost:5000/reviews');
+  //   const data = await res.json();
+  //   return data
+  // }
 
-  const fetchReviews = async () => {
-    const reviewData = await getReviews();
-    setReviews(reviewData);
-  }
-  //Invoke Data Fetch
-  useEffect(()=> {
-    fetchReviews();
-  }, [])
+  // const fetchReviews = async () => {
+  //   const reviewData = await getReviews();
+  //   setReviews(reviewData);
+  // }
+  // //Invoke Data Fetch
+  // useEffect(()=> {
+  //   fetchReviews();
+  // }, [])
+
+  // const [index, setIndex] = useState(0);
+  // console.log(reviews[0])
+  // const {name, job, image, text} = reviews[0]
+  // console.log(name,job,image,text)
 
   return (
     <main className="container">
       <h1 className="heading">Our Reviews:</h1>
-      {reviews.map((review) => {
-        return <Review key = {review.id} data={review}/>
-      })}
+      <Review />
     </main>
   );
 }
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,16 +1,52 @@
+import {useState, useEffect} from 'react';
 import {BsChevronDoubleLeft, BsChevronDoubleRight} from 'react-icons/bs'
+import reviews from './data'
 
-function Review({ data }) {
+function Review() {
+    const [index, setIndex] = useState(0);
+    const {name, job,image, text} = reviews[index]
+
+    const checkNumber = (num) => {
+        if(num > reviews.length-1){
+            return 0;
+        }
+        if(num < 0){
+            return reviews.length-1
+        }
+        return num
+    }
+
+    const onClickLeft = () => {
+            
+        setIndex((index)=> {
+          let newIndex = index - 1;  
+          return checkNumber(newIndex)
+        })
+    }
+
+    const onClickRight = () => {
+        
+        setIndex((index)=> {
+            let newIndex = index +1;  
+            return checkNumber(newIndex)
+          })
+      }
+
+      const surpriseMe = () => {
+          setIndex(()=> {
+              return Math.floor(Math.random()*4);
+          })
+      }
     return <article className="review-container">
-        <img src={data.image} alt="Review Author" className="review-author-photo"/>
-        <h3 className="review-author">{data.name}</h3>
-        <h3 className="review-job">{data.job}</h3>
-        <p className="review-text">{data.text}</p>
+        <img src={image} alt="Review Author" className="review-author-photo"/>
+        <h3 className="review-author">{name}</h3>
+        <h3 className="review-job">{job}</h3>
+        <p className="review-text">{text}</p>
         <div className="nav-icons">
-            <BsChevronDoubleLeft className="nav-btns"/>
-            <BsChevronDoubleRight className="nav-btns"/>
+            <BsChevronDoubleLeft className="nav-btns" onClick={onClickLeft}/>
+            <BsChevronDoubleRight className="nav-btns" onClick={onClickRight}/>
         </div>
-        <button className="btn">Surprise Me</button>
+        <button className="btn" onClick={surpriseMe}>Surprise Me</button>
     </article>
 }
 
